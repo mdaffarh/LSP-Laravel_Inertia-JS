7801906f0cdca0e8f8c8e66e802b2b8f3ca683dd
mdaffarh <daffarizmawan@gmail.com>
2023-01-20 15:30:45 +0700

    nilai

--- resources/views/nilai/index.blade.php
+++ resources/views/nilai/index.blade.php
@@ -3,7 +3,9 @@
     <center>
         <b>
             <h2>LIST DATA NILAI</h2>
-            <a href="/nilai/create" class="button-primary">TAMBAH DATA</a>
+            @if (session('user')->role == 'guru')
+                <a href="/nilai/create" class="button-primary">TAMBAH DATA</a>
+            @endif
             @if (session('success'))
                 <p class="text-success">{{ session('success') }}</p>
             @endif
@@ -20,23 +22,40 @@
                     <th>UTS</th>
                     <th>UAS</th>
                     <th>NA</th>
-                    <th>ACTION</th>
+                    @if (session('user')->role == 'guru')
+                        <th>ACTION</th>
+                    @endif
                 </tr>
                 @foreach ($nilai as $n)     
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $n->mengajar->guru->nama_guru }}</td>
-                        <td>{{ $n->mengajar->mapel->nama_mapel }}</td>
-                        <td>{{ $n->siswa->nama_siswa }}</td>
-                        <td>{{ $n->uh }}</td>
-                        <td>{{ $n->uts }}</td>
-                        <td>{{ $n->uas }}</td>
-                        <td>{{ $n->na }}</td>
-                        <td> 
-                            <a href="/nilai/edit/{{ $n->id }}" class="button-warning">EDIT</a>
-                            <a href="/nilai/destroy/{{ $n->id }}" class="button-danger" onclick="return confirm('Anda Akan Menghapus Data Ini?')">DELETE</a>
-                        </td>
-                    </tr>
+                    @if (session('user')->role == 'siswa')
+                        @if (session('user')->nis == $n->siswa->nis)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $n->mengajar->guru->nama_guru }}</td>
+                                <td>{{ $n->mengajar->mapel->nama_mapel }}</td>
+                                <td>{{ $n->siswa->nama_siswa }}</td>
+                                <td>{{ $n->uh }}</td>
+                                <td>{{ $n->uts }}</td>
+                                <td>{{ $n->uas }}</td>
+                                <td>{{ $n->na }}</td>
+                            </tr>
+                        @endif
+                    @else
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $n->mengajar->guru->nama_guru }}</td>
+                            <td>{{ $n->mengajar->mapel->nama_mapel }}</td>
+                            <td>{{ $n->siswa->nama_siswa }}</td>
+                            <td>{{ $n->uh }}</td>
+                            <td>{{ $n->uts }}</td>
+                            <td>{{ $n->uas }}</td>
+                            <td>{{ $n->na }}</td>
+                            <td> 
+                                <a href="/nilai/edit/{{ $n->id }}" class="button-warning">EDIT</a>
+                                <a href="/nilai/destroy/{{ $n->id }}" class="button-danger" onclick="return confirm('Anda Akan Menghapus Data Ini?')">DELETE</a>
+                            </td>
+                        </tr>
+                    @endif
                 @endforeach
             </table>
         </b>
